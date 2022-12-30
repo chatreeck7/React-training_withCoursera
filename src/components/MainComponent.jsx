6ffb2c9f6ea637205f6489a378b941dbb1dd5c1c
chatree kurupakorn <chatree.devl@gmail.com>
2022-12-30 14:41:13 +0700

    complete SPA or Single Page Application parrt I

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -4,7 +4,11 @@ import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes'
+import { COMMENTS } from '../shared/comments'
+import { PROMOTIONS } from '../shared/promotions'
+import { LEADERS } from '../shared/leaders'
 import { Switch, Redirect, Route } from 'react-router-dom';
 // Add a Container Component
 class Main extends Component {
@@ -13,6 +17,9 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS,
       selectedDish: null
     };
   }
@@ -24,7 +31,11 @@ class Main extends Component {
   render(){
     const HomePage = () => {
         return (
-            <Home />
+            <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+            />
         );
     }
     return (
@@ -34,6 +45,7 @@ class Main extends Component {
             <Switch>
                 <Route path="/home" component={HomePage}/>
                 <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                <Route exact path="/contactus" component={Contact}/>
                 <Redirect to="/home" /> {/* Redirect to home if path is not in configuration router */}
             </Switch>
             <Footer />
