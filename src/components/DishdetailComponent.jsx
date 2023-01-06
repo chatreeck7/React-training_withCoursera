f2112f5c723898026d3a1f28113a9d3e71dfd749
chatree kurupakorn <chatree.devl@gmail.com>
2023-01-06 11:36:48 +0700

    React Animation Components

--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom"
 import { Control, LocalForm, Errors } from "react-redux-form"
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -93,21 +94,24 @@ class CommentForm extends Component {
 function RenderComments({comments, postComment, dishId}) {
     if(comments != null) {
         // console.log(dish.comments);
-        const comment = comments.map(({author, comment, date, id, rating}) => {                    
+        const comment = 
+            comments.map(({author, comment, date, id, rating}) => {                    
             // let dateformat = new Date(date);
             // console.log('Date format: ', dateformat.getTime());
             return (    
-                    <li key={id}>
-                        <p>{comment}</p>
-                        <p>-- {author},
-                        &nbsp;
-                        {new Intl.DateTimeFormat('en-US', {
-                            year: 'numeric',
-                            month: 'short',
-                            day: '2-digit'
-                        }).format(new Date(Date.parse(date)))}
-                        </p>
-                    </li>
+                    <Fade in>
+                        <li key={id}>
+                            <p>{comment}</p>
+                            <p>-- {author},
+                            &nbsp;
+                            {new Intl.DateTimeFormat('en-US', {
+                                year: 'numeric',
+                                month: 'short',
+                                day: '2-digit'
+                            }).format(new Date(Date.parse(date)))}
+                            </p>
+                        </li>
+                    </Fade>
             );
         });
 
@@ -115,7 +119,9 @@ function RenderComments({comments, postComment, dishId}) {
             <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
                 <ul className="list-unstyled">
-                    { comment }
+                    <Stagger in>
+                        { comment }
+                    </Stagger>
                 </ul>
                 <CommentForm dishId={dishId} postComment={postComment}/>
             </div>
@@ -131,13 +137,19 @@ function RenderComments({comments, postComment, dishId}) {
 function RenderDish({dish}) {
     if(dish != null) {
         return(
-            <Card>
-                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>                 
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>                 
+            </FadeTransform>
         );
     }
     else {
